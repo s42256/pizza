26c357360723d3fffd5beb0cd899e13e6a00c8db
MD SAKIB UL ISLAM <12296309@cqumail.com>
2025-10-03 10:24:35 +1000

    feat(phase2): OrderEvent and ReportEvent (arrival generator and placeholder report)

--- src/main/java/cqu/pizza/lifecycle/events/OrderEvent.java
+++ src/main/java/cqu/pizza/lifecycle/events/OrderEvent.java
@@ -4,10 +4,31 @@
  */
 package cqu.pizza.lifecycle.events;
 
+import cqu.pizza.lifecycle.Model;
+import cqu.pizza.lifecycle.data.Request;
+import cqu.pizza.simulator.Event;
+import cqu.pizza.simulator.ISchedule;
+
 /**
  *
  * @author sisak
  */
-public class OrderEvent {
-    
+public class OrderEvent extends Event {
+
+    private final Request request;
+
+    public OrderEvent(Request request) {
+        super(request.orderTime());
+        this.request = request;
+    }
+
+    @Override
+    public void process(Model m, ISchedule s) {
+        // create/action the current order (may be refused)
+        m.order(request);
+
+        // schedule the next request arrival
+        Request next = m.nextRequest();
+        s.schedule(new OrderEvent(next));
+    }
 }
--- src/main/java/cqu/pizza/lifecycle/events/ReportEvent.java
+++ src/main/java/cqu/pizza/lifecycle/events/ReportEvent.java
@@ -4,10 +4,22 @@
  */
 package cqu.pizza.lifecycle.events;
 
+import cqu.pizza.lifecycle.Model;
+import cqu.pizza.simulator.Event;
+import cqu.pizza.simulator.ISchedule;
+
 /**
  *
  * @author sisak
  */
-public class ReportEvent {
-    
+public class ReportEvent extends Event {
+
+    public ReportEvent(int time) {
+        super(time);
+    }
+
+    @Override
+    public void process(Model m, ISchedule s) {
+        System.out.printf("Report to be generated at time %d - under development%n", getTime());
+    }
 }
